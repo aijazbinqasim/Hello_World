c97ad8dfa069550e8c5131abd43bf5bcaaf9c423
aijazbinqasim <aijazbinqasim@hotmail.com>
2023-09-13 19:30:10 +0500

    Learn and practice of topic: [View Component]

---
+++ Hello_World/Components/ProgramingLangViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hello_World.Models;
+using System.Collections.Generic;
+
+namespace Hello_World.Components
+{
+    public class ProgramingLangViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var plang = new PLang()
+            {
+                Names = new List<string> { 
+                    "C#", 
+                    "PHP", 
+                    "JavaScript", 
+                    "Java", 
+                    "Dart", 
+                    "JQuery", 
+                    "Python" 
+                }
+            };
+
+            return View("Plangs", plang);
+        }
+    }
+}
---
+++ Hello_World/Models/PLang.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Hello_World.Models
+{
+    public class PLang
+    {
+        public List<string> Names { get; set; }
+    }
+}
